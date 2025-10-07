50903cd678bcd2c02df76687a02e5a2121572d1b
kei nishikawa <>
2025-10-07 11:19:28 +0900

    src 追加

---
+++ js/settings.js
@@ -0,0 +1,15 @@
+// おみくじ結果リスト
+const omikujiResults = [
+  { type: "大吉", text: "最高の一日になります！", img: "images/result/daikichi.png" },
+  { type: "中吉", text: "いいことがあるかも！", img: "images/result/chukichi.png" },
+  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/shokichi.png" },
+  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/kyo.png" }
+];
+
+// ラッキーアイテム
+const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
+
+// ラッキーアイテムON/OFF
+const settings = {
+  showLuckyItem: true
+};
