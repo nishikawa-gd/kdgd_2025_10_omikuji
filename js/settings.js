3d9345caec0e7802908334e3b076cae5b1630f4e
春名香里 <>
2025-10-14 12:03:05 +0900

    変更

--- js/settings.js
+++ js/settings.js
@@ -1,15 +1,31 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "大吉", text: "最高の一日になります！", img: "images/result/daikichi.png" },
-  { type: "中吉", text: "いいことがあるかも！", img: "images/result/chukichi.png" },
-  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/shokichi.png" },
-  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/kyo.png" }
+  {
+    type: "大吉",
+    text: "何かいいこと起こるかも！チャンスのとき！",
+    img: "images/result/daikiti.png"
+  },
+  {
+    type: "中吉",
+    text: "今日のあなた、けっこうツイてるかも！",
+    img: "images/result/tyuukiti.png"
+  },
+  {
+    type: "小吉",
+    text: "ちっちゃい幸せ、見つけられるかも!",
+    img: "images/result/syoukiti.png"
+  },
+  {
+    type: "凶",
+    text: "よくないことが起こるかも　今日は1日大人しくしていよう。",
+    img: "images/result/kyou.png"
+  }
 ];
 
-// ラッキーアイテム
-const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
+// ラッキーアイテム（例）
+const luckyItems = ["だるま", "お茶", "お守り", "木づち", "お菓子", "ねこグッズ"];
 
-// ラッキーアイテムON/OFF
+// 設定
 const settings = {
   showLuckyItem: true
 };
