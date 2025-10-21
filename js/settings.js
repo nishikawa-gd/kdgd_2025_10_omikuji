62e9856cfda6b128f0e1feee18b9ea30531e1320
ウタシロユリ <>
2025-10-21 14:37:46 +0900

    Revert "全体作業"
    This reverts commit cad54b6bcebe9aa711765d7553e03d4950399ca4.

--- js/settings.js
+++ js/settings.js
@@ -1,24 +1,15 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "大当たり！", text: "カゴいっぱいのおかしをどうぞ！", img: "images/result/lucky.png" },
-  { type: "当たり！", text: "お菓子をいくつかゲット！まあまあの幸運", img: "images/result/good.png" },
-  { type: "まぁまぁ", text: "お菓子をひとつゲットした！", img: "images/result/well.png" },
-  { type: "大ハズレ", text: "イタズラされた！", img: "images/result/bad.png" }
-];
-
-// キャラクター画像
-const omikujiCharacter = [
-  { img: "images/result/franken2.png" },
-  { img: "images/result/pumpkin2.png" },
-  { img: "images/result/ghost3.png" },
-  { img: "images/result/ghost4.png" },
-  { img: "images/result/dracula2.png" }
+  { type: "大吉", text: "最高の一日になります！", img: "images/result/daikichi.png" },
+  { type: "中吉", text: "いいことがあるかも！", img: "images/result/chukichi.png" },
+  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/shokichi.png" },
+  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/kyo.png" }
 ];
 
 // ラッキーアイテム
-const luckyItems = ["ランタン", "小説", "キャンディ", "鍵", "猫のキーホルダー", "お茶"];
+const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
 
-// 表示設定
+// ラッキーアイテムON/OFF
 const settings = {
   showLuckyItem: true
 };
