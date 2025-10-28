f6cb1aece445e4acf7244774503a2030513c7d0d
友藤宇衣香 <>
2025-10-28 11:23:49 +0900

    結果ページと履歴ページ調整

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,13 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "", text: "", img: "images/result/daikichi.png" },
-  { type: "", text: "", img: "images/result/chukichi.png" },
-  { type: "", text: "", img: "images/result/shokichi.png" },
-  { type: "", text: "", img: "images/result/kyo.png" }
+  { type: "パピヨン", text: "柔らかい気持ちで変化を楽しんでみて！️", img: "images/result/daikichi.png" },
+  { type: "ゴールデンレトリーバー", text: "太陽のような笑顔で過ごしてみて！", img: "images/result/chukichi.png" },
+  { type: "柴", text: "小さな努力を大切に️！", img: "images/result/shokichi.png" },
+  { type: "コリー", text: "目標に向かって計画を立てると運気上昇！", img: "images/result/kyo.png" }
 ];
 
 // ラッキーアイテム
-const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
+const luckyItems = ["ブレスレット", "カラーペン", "リボン", "ミニミラー", "スニーカー", "お茶"];
 
 // ラッキーアイテムON/OFF
 const settings = {
